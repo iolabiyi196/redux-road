3cb21c6848a827d801c846b2a32e5b9cf5cd4e11
iolabiyi <79932461+iolabiyi@users.noreply.github.com>
2022-12-26 20:16:20 -0500

    reformatted

--- redux-road.js
+++ redux-road.js
@@ -3,97 +3,97 @@ const initialWagonState = {
   distance: 0,
   days: 0,
   cash: 200,
-};
+}
 
 const reducer = (state = initialWagonState, action) => {
-  let { supplies, distance, days } = state;
+  let { supplies, distance, days } = state
   switch (action.type) {
-    case "gather": {
-      supplies += 15;
-      days++;
+    case 'gather': {
+      supplies += 15
+      days++
       return {
         ...state,
         supplies,
         days,
-      };
+      }
     }
-    case "travel": {
-      supplies -= 20 * action.payload;
-      distance += 10 * action.payload;
-      days += action.payload;
+    case 'travel': {
+      supplies -= 20 * action.payload
+      distance += 10 * action.payload
+      days += action.payload
 
       if (supplies < 0) {
-        return state;
+        return state
       } else {
         return {
           ...state,
           supplies,
           distance,
           days,
-        };
+        }
       }
     }
-    case "tippedWagon": {
-      supplies -= 30;
-      days++;
+    case 'tippedWagon': {
+      supplies -= 30
+      days++
       return {
         ...state,
         supplies,
         days,
-      };
+      }
     }
-    case "sell": {
-      supplies -= 20;
-      cash += 5;
+    case 'sell': {
+      supplies -= 20
+      cash += 5
       return {
         ...state,
         supplies,
-        cash
-      };
+        cash,
+      }
     }
-    case "buy": {
-      supplies += 25;
-      cash -= 15;
+    case 'buy': {
+      supplies += 25
+      cash -= 15
       return {
         ...state,
         supplies,
-        cash
-      };
+        cash,
+      }
     }
-    case "theft": {
-      cash /= 2;
+    case 'theft': {
+      cash /= 2
       return {
         ...state,
-        cash
-      };
+        cash,
+      }
     }
     default: {
-      return state;
+      return state
     }
   }
-};
+}
 
-let wagon = reducer(undefined, {});
-console.log(wagon);
+let wagon = reducer(undefined, {})
+console.log(wagon)
 
 wagon = reducer(wagon, {
-  type: "travel",
+  type: 'travel',
   payload: 1,
-});
-console.log(wagon);
+})
+console.log(wagon)
 
 wagon = reducer(wagon, {
-  type: "gather",
-});
-console.log(wagon);
+  type: 'gather',
+})
+console.log(wagon)
 
 wagon = reducer(wagon, {
-  type: "tippedWagon",
-});
-console.log(wagon);
+  type: 'tippedWagon',
+})
+console.log(wagon)
 
 wagon = reducer(wagon, {
-  type: "travel",
+  type: 'travel',
   payload: 3,
-});
-console.log(wagon);
+})
+console.log(wagon)
